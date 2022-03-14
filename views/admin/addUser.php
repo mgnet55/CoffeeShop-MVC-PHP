aec613ab23396e2ce320ec7388cf31575cdf1d9e
DreamBig12 <sciencefi2@gmail.com>
2022-03-14 19:16:55 +0200

    admin updates

---
+++ views/admin/addUser.php
@@ -0,0 +1,41 @@
+<form method="post" action="sign.php" enctype="multipart/form-data">
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="inputEmail4" placeholder="Name" name="name">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="inputEmail4" placeholder="Email" name="email">
+
+          </div>
+          <div class="form-group col-md-6">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="inputPassword4" placeholder="Password" name="password">
+          </div>
+        </div>  
+          <div class="form-group col-md-6">
+            <label for="confirm">Confirm Password</label>
+            <input type="password" class="form-control" id="inputPassword4" placeholder="Password" name="confirm">
+          </div>
+          <div class="form-group col-md-4">
+            <label for="Room">Room no.</label>
+            <input type="text"class="form-control" id="inputRoom" placeholder="Room" name="Room">
+          </div>
+          <div class="form-group col-md-4">
+             <label for="ext">Ext</label>
+             <input type="text" class="form-control" id="" name="ext">
+          </div>
+          <div class="form-group col-md-2">
+            <label for="photo">Profile picture</label>
+            <input type="file" class="form-control" id="" name="photo">
+          </div>
+          <div class="form-group col-md-4">
+            <label for="isAdmin">Admin</label>
+            <select id="isAdmin" class="form-control" name="isAdmin">
+              <option value ="true">True</option>
+              <option value ="false">"false</option>
+            </select>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary m-2" name="sign">Sign in</button>
---
+++ views/admin/manual_orders.php
@@ -0,0 +1,82 @@
+<div class="home-user">
+        <div class="search">
+          <div class="container">
+             <select name="" id="Select" class=""form-select" aria-label="Default select example"">
+              <option selected>Users</option>
+               <option value="">First user</option>
+               <option value="">First user</option>
+               <option value="">First user</option>
+             </select>
+             <form action="">
+              <input type="text" name="search" id="search">
+              <input type="submit" value="Search" id="submit">
+             </form>
+          </div>
+        </div>
+        <div class="content">
+          <div class="container">
+            <div class="product">
+              <img src="images/products/coffee1.png">
+              <p class="name">Coffee <i class="fa-solid fa-coffee-beans"></i></p>
+              <p class="Price">$2.00</p>
+              <a href="" class="btn2">Order Now</a>
+            </div>
+            <div class="product">
+              <img src="images/products/tea.png">
+              <p class="name">tea</p>
+              <p class="Price">$2.00</p>
+              <a href="" class="btn2">Order Now</a>
+            </div>
+            <div class="product">
+              <img src="images/products/cuppochino.png">
+              <p class="name">Cuppochinoo</p>
+              <p class="Price">$2.00</p>
+              <a href="" class="btn2">Order Now</a>
+            </div>
+            <div class="product">
+              <img src="images/products/dessert.png">
+              <p class="name">Coffee</p>
+              <p class="Price">$2.00</p>
+              <a href="" class="btn2">Order Now</a>
+            </div>
+            <div class="product">
+              <img src="images/products/exspresso.png">
+              <p class="name">Coffee</p>
+              <p class="Price">$2.00</p>
+              <a href="" class="btn2">Order Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="cart" id="cart">
+          <div class="icon" id="icon">
+            cart
+         </div>
+         <form action="">
+          <div class="child">
+            <span>Product name</span>
+            <input type="number">
+            <span>Product price</span>
+            <a href="">Remove</a>
+          </div>
+          <div class="child">
+           <span>Product name</span>
+           <input type="number">
+           <span>Product price</span>
+           <a href="">Remove</a>
+         </div>
+         <div class="note">
+           <textarea name="" id=""" rows="5" placeholder="Notes"></textarea>
+           <label for="combo">Room</label>
+           <select name="combo" id="">
+             <option value="comboBox">ComboBox</option>
+             <option value="other">Other</option>
+           </select>
+           <br>
+           <hr>
+           <br>
+           <p>Total Price: </p>
+           <input type="submit" class="btn2">
+         </div>
+         </form>
+        </div>
+       </div>
