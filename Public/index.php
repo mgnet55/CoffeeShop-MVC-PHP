6540d71bf78c9bdaa8594c2e399c63b7ddc414cc
Mina Gamal <mena.g2013@gmail.com>
2022-03-14 22:36:29 +0200

    session set autostart

--- Public/index.php
+++ Public/index.php
@@ -2,6 +2,7 @@
 
 use Dotenv\Dotenv;
 
+ini_set('session.auto_start', 1);
 require_once __DIR__ . DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Support'.DIRECTORY_SEPARATOR.'helpers.php';
 require_once BASE_PATH . 'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 require_once BASE_PATH . 'routes'.DIRECTORY_SEPARATOR.'web.php';
